c75e02e652a19a2c5f0ce75df56292ba7ee910a9
Vi[Told] <vitalya_migun@mail.ru>
2023-03-11 13:12:37 +0900

    Custom user models, station model, roles model.

--- projectx/railapp/models.py
+++ projectx/railapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class Roles(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+
+
+class CustomUser(AbstractUser):
+    role = models.ForeignKey(Roles, on_delete=models.SET_NULL, null=True, blank=False)
+
+
+class Stations(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    start_date = models.DateTimeField(null=False, blank=False)
+    end_date = models.DateTimeField(null=False, blank=False)
