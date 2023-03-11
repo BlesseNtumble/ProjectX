0e7760ee36fd2ab3f505423f7fedf2a5e337f34e
Vi[Told] <vitalya_migun@mail.ru>
2023-03-11 14:17:28 +0900

    Играемся с админкой и моделями.

--- projectx/railapp/admin.py
+++ projectx/railapp/admin.py
@@ -1,3 +1,67 @@
+from django.contrib import messages
 from django.contrib import admin
 
 # Register your models here.
+from railapp.models import *
+
+
+@admin.register(CustomUser)
+class AdminCustomUser(admin.ModelAdmin):
+    list_display = ['username', 'get_role', 'is_staff', 'is_active']
+    list_display_links = list_display
+    search_fields = list_display
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        form.base_fields['role'].label = 'Роль'
+        return form
+
+    def get_role(self, obj):
+        result = Roles.objects.filter(id=obj.role_id).first()
+        return result
+
+    get_role.short_description = 'Роль'
+
+@admin.register(Roles)
+class AdminRoles(admin.ModelAdmin):
+    list_display = ['__str__']
+    list_display_links = list_display
+    search_fields = list_display
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        form.base_fields['name'].label = 'Название роли'
+        return form
+
+@admin.register(Stations)
+class AdminStations(admin.ModelAdmin):
+    list_display = ['get_name', 'get_start_date', 'get_end_date']
+    list_display_links = list_display
+    search_fields = list_display
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        form.base_fields['name'].label = 'Название станции'
+        form.base_fields['start_date'].label = 'Дата прибытия'
+        form.base_fields['end_date'].label = 'Дата отправки'
+        return form
+
+    def get_name(self, obj):
+        return obj.name
+
+    def get_start_date(self, obj):
+        return obj.start_date
+
+    def get_end_date(self, obj):
+        return obj.end_date
+
+    get_name.short_description = 'Название'
+    get_start_date.short_description = 'Дата прибытия'
+    get_end_date.short_description = 'Дата отправки'
+
+    def save_model(self, request, obj, form, change):
+        if obj.start_date > obj.end_date:
+            self.message_user(request, 'Дата прибытия не может быть больше даты отправки!', messages.ERROR)
+            return
+
+        obj.save()
--- projectx/railapp/models.py
+++ projectx/railapp/models.py
@@ -7,6 +7,12 @@ from django.contrib.auth.models import AbstractUser
 class Roles(models.Model):
     name = models.CharField(max_length=128, null=False, blank=False)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
 
 class CustomUser(AbstractUser):
     role = models.ForeignKey(Roles, on_delete=models.SET_NULL, null=True, blank=False)
@@ -16,3 +22,10 @@ class Stations(models.Model):
     name = models.CharField(max_length=256, null=False, blank=False)
     start_date = models.DateTimeField(null=False, blank=False)
     end_date = models.DateTimeField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Станция'
+        verbose_name_plural = 'Станции'
