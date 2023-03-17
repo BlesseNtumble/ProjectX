62388159159aa9d8bac5eb75b6e57fd148811b21
DVN4ENKX <serzh.fedotov@list.ru>
2023-03-17 22:19:10 +0900

    add auth and func

--- projectx/railapp/urls.py
+++ projectx/railapp/urls.py
@@ -5,9 +5,9 @@ from railapp.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('logout', logout_page, name='logout'),
-    path('login', LoginUser.as_view(), name='login'),
-    path('chat', name='chat'),
-    path('profile', name='profile'),
-    path('routes', name='routes'),
-    path('settings', name='settings'),
+    path('login', auth, name='login'),
+    path('chat', chat, name='chat'),
+    path('profile', profile, name='profile'),
+    path('routes', routes, name='routes'),
+    path('settings', settings, name='settings'),
 ]
--- projectx/railapp/views.py
+++ projectx/railapp/views.py
@@ -22,9 +22,39 @@ def index(request):
 
 
 @login_required(login_url='/login')
+def chat(request):
+    context = {'title': 'Чат'}
+    return render(request, template + '/chat.html', context=context)
+
+
+@login_required(login_url='/login')
+def profile(request):
+    context = {'title': 'Профиль'}
+    return render(request, template + '/profile.html', context=context)
+
+
+@login_required(login_url='/login')
+def routes(request):
+    context = {'title': 'Маршрут следования'}
+    return render(request, template + '/routes.html', context=context)
+
+
+@login_required(login_url='/login')
+def settings(request):
+    context = {'title': 'Настройки'}
+    return render(request, template + '/settings.html', context=context)
+
+
+@login_required(login_url='/login')
 def logout_page(request):
     logout(request)
     return redirect('login')
+
+
+def auth(request):
+    f = RegisterForms.LoginUserForm
+    context = {'form':f, 'title':'Авторизация'}
+    return render(request, template + '/auth.html', context=context)
 
 
 class LoginUser(LoginView):
