436c29ca141bf8a05957715cb3826c1d1c404fff
Vi[Told] <BlesseNtumble@users.noreply.github.com>
2023-03-17 21:23:21 +0900

    убрал приколы

--- projectx/railapp/forms.py
+++ projectx/railapp/forms.py
@@ -7,15 +7,15 @@ from railapp.models import CustomUser
 class RegisterForms(forms.ModelForm):
 
     class RegisterUserForm(UserCreationForm):
-        username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'value placeholder': 'Логин'}))
-        email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'value placeholder': 'Email адрес'}))
-        password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'value placeholder': 'Пароль'}))
-        password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-control', 'value placeholder': 'Повтор пароля'}))
+        username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Логин'}))
+        email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email адрес'}))
+        password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+        password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повтор пароля'}))
 
         class Meta:
             model = CustomUser
             fields = ('username', 'email', 'password1', 'password2')
 
     class LoginUserForm(AuthenticationForm):
-        username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'value placeholder': 'Логин'}))
-        password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'value placeholder': 'Пароль'}))
+        username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Логин'}))
+        password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
