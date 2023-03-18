ab21dfdbc7a4c01d0a070694d0ed77616aba7d4f
BlesseNtumble <BlesseNtumble@users.noreply.github.com>
2023-03-18 11:03:32 +0900

    change stations list, admin models

--- projectx/railapp/admin.py
+++ projectx/railapp/admin.py
@@ -35,29 +35,19 @@ class AdminRoles(admin.ModelAdmin):
 
 @admin.register(Stations)
 class AdminStations(admin.ModelAdmin):
-    list_display = ['get_name', 'get_start_date', 'get_end_date']
+    list_display = ['get_name']
     list_display_links = list_display
     search_fields = ['name']
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
         form.base_fields['name'].label = 'Название станции'
-        form.base_fields['start_date'].label = 'Дата прибытия'
-        form.base_fields['end_date'].label = 'Дата отправки'
         return form
 
     def get_name(self, obj):
         return obj.name
 
-    def get_start_date(self, obj):
-        return obj.start_date
-
-    def get_end_date(self, obj):
-        return obj.end_date
-
     get_name.short_description = 'Название'
-    get_start_date.short_description = 'Дата прибытия'
-    get_end_date.short_description = 'Дата отправки'
 
     def save_model(self, request, obj, form, change):
         if obj.start_date > obj.end_date:
@@ -68,9 +58,24 @@ class AdminStations(admin.ModelAdmin):
 
 @admin.register(StationList)
 class AdminStationList(admin.ModelAdmin):
-    list_display = ['list_id', 'station']
+    list_display = ['list_id', 'station', 'get_start_date', 'get_end_date']
     list_display_links = list_display
     search_fields = list_display
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        form.base_fields['start_date'].label = 'Дата прибытия'
+        form.base_fields['end_date'].label = 'Дата отправки'
+        return form
+
+    def get_start_date(self, obj):
+        return obj.start_date
+
+    def get_end_date(self, obj):
+        return obj.end_date
+
+    get_start_date.short_description = 'Дата прибытия'
+    get_end_date.short_description = 'Дата отправки'
 
 @admin.register(ChatList)
 class AdminChatList(admin.ModelAdmin):
--- projectx/railapp/models.py
+++ projectx/railapp/models.py
@@ -20,8 +20,6 @@ class CustomUser(AbstractUser):
 
 class Stations(models.Model):
     name = models.CharField(max_length=256, null=False, blank=False)
-    start_date = models.DateTimeField(null=False, blank=False)
-    end_date = models.DateTimeField(null=False, blank=False)
 
     def __str__(self):
         return f'{self.name}'
@@ -34,6 +32,8 @@ class Stations(models.Model):
 class StationList(models.Model):
     list_id = models.IntegerField(null=False, blank=False)
     station = models.ForeignKey(Stations, on_delete=models.CASCADE, null=False, blank=False)
+    start_date = models.DateTimeField(null=False, blank=False)
+    end_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f'[{self.list_id}] {self.station}'
