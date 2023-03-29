6bee3f826db898ed477bd1b54931fb45af1c11d1
DVN4ENKX <serzh.fedotov@list.ru>
2023-03-29 23:36:33 +0900

    Полировка дизайна по требованиям заказчика

--- projectx/railapp/forms.py
+++ projectx/railapp/forms.py
@@ -7,15 +7,21 @@ from railapp.models import CustomUser
 class RegisterForms(forms.ModelForm):
 
     class RegisterUserForm(UserCreationForm):
-        username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Логин'}))
-        email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email адрес'}))
-        password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
-        password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повтор пароля'}))
+        username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control m-2',
+                                                                 'style': 'border-color:#C52E32;', 'placeholder': 'Логин'}))
+        email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control m-2',
+                                                                'style': 'border-color:#C52E32;', 'placeholder': 'Email адрес'}))
+        password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control m-2',
+                                                                      'style': 'border-color:#C52E32;', 'placeholder': 'Пароль'}))
+        password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control m-2',
+                                                                      'style': 'border-color:#C52E32;', 'placeholder': 'Повтор пароля'}))
 
         class Meta:
             model = CustomUser
             fields = ('username', 'email', 'password1', 'password2')
 
     class LoginUserForm(AuthenticationForm):
-        username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Логин'}))
-        password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+        username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control m-2',
+                                                                 'style': 'border-color:#C52E32;', 'placeholder': 'Логин'}))
+        password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control m-2',
+                                                                     'style': 'border-color:#C52E32;', 'placeholder': 'Пароль'}))
